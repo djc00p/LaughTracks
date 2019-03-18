02cfe4f511d04f21f8cb2e0a4f179efee0df24d4
Deonte Cooper <deonte.cooper12@gmail.com>
2019-03-18 07:13:32 -0600

    Added class methods average age and list of cities

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -3,4 +3,17 @@ class Comedian < ActiveRecord::Base
   validates :name, presence: true
   validates :age, presence: true
   validates :city, presence: true
+
+  def self.average_comedian_age
+    average(:age).to_i
+  end
+
+  def self.list_of_cities
+    list = ""
+    cities = Comedian.distinct.pluck(:city)
+    cities.each do |city|
+      list << city + ", "
+    end
+    list[0..(list.length - 3)]
+  end
 end
